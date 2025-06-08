5008d2871e71dcdc09fb2bd29d9edd931b73d34b
aashish <aashishkumar1812@gmail.com>
2025-06-08 13:16:27 +0530

    p link

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,118 +1,130 @@
 import { ArrowRight, Github } from "lucide-react";
 
 const projects = [
-	{
-		id: 1,
-		title: "ExpenseEase",
-		description:
-			"A personal finance tracker to manage expenses and budgets effectively.",
-		image: "/projects/Projectimage1.png",
-		tags: [
-			"React",
-			"CSS",
-			"TypeScript",
-			"Clerk",
-			"MongoDB",
-			"Express",
-			"Node.js",
-		],
-		githubUrl: "https://github.com/Aashishkr2003/Expense_Ease",
-		liveUrl: "https://expense-ease-fe.onrender.com/",
-	},
-	{
-		id: 2,
-		title: "Market Segmentation ",
-		description:
-			"An analysis of customer data to identify distinct market segments using ML.",
-		image: "/projects/Projectimage2.png",
-		tags: ["Python","Machine learning","Pandas","Matplotlib", "Google Colab"],
-		githubUrl: "https://github.com/Aashishkr2003/MarketSegmentation",
-	},
-	{
-		id: 3,
-		title: "NFC-Ticketing System",
-		description:
-			"Full-featured e-commerce platform with user authentication and payment processing.",
-		image: "/projects/Projectimage3.png",
-		tags: ["Kotlin", "Android Studio ", "ADB", "NfcAdapter", "HCE"],
-		githubUrl: "#",
-	},
+  {
+    id: 1,
+    title: "ExpenseEase",
+    description:
+      "A personal finance tracker to manage expenses and budgets effectively.",
+    image: "/projects/Projectimage1.png",
+    tags: [
+      "React",
+      "CSS",
+      "TypeScript",
+      "Clerk",
+      "MongoDB",
+      "Express",
+      "Node.js",
+    ],
+    githubUrl: "https://github.com/Aashishkr2003/Expense_Ease",
+    liveUrl: "https://expense-ease-fe.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "Market Segmentation ",
+    description:
+      "An analysis of customer data to identify distinct market segments using ML.",
+    image: "/projects/Projectimage2.png",
+    tags: [
+      "Python",
+      "Machine learning",
+      "Pandas",
+      "Matplotlib",
+      "Google Colab",
+    ],
+    githubUrl: "https://github.com/Aashishkr2003/MarketSegmentation",
+  },
+  {
+    id: 3,
+    title: "NFC-Ticketing System",
+    description:
+      "Full-featured e-commerce platform with user authentication and payment processing.",
+    image: "/projects/Projectimage3.png",
+    tags: ["Kotlin", "Android Studio ", "ADB", "NfcAdapter", "HCE"],
+    githubUrl: "#",
+  },
 ];
 
 export const ProjectsSection = () => {
-	return (
-		<section id="projects" className="py-24 px-4 relative">
-			<div className="container mx-auto max-w-5xl">
-				<h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-					{" "}
-					Featured{" "}
-					<span className="text-primary"> Projects </span>
-				</h2>
+  return (
+    <section id="projects" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          {" "}
+          Featured <span className="text-primary"> Projects </span>
+        </h2>
 
-				<p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
-					Here are some of my recent projects. Each project was carefully
-					crafted with attention to detail, performance, and user experience.
-				</p>
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some of my recent projects. Each project was carefully
+          crafted with attention to detail, performance, and user experience.
+        </p>
 
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-					{projects.map((project, key) => (
-						<div
-							key={key}
-							className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
-						>
-							<div className="h-48 overflow-hidden">
-								<img
-									src={project.image}
-									alt={project.title}
-									className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-								/>
-							</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              <div className="h-48 overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
 
-							<div className="p-6">
-								<div className="flex flex-wrap gap-2 mb-4">
-									{project.tags.map((tag, idx) => (
-										<span
-											key={idx}
-											className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground"
-										>
-											{tag}
-										</span>
-									))}
-								</div>
+              <div className="p-6">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.tags.map((tag, idx) => (
+                    <span
+                      key={idx}
+                      className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
 
-								<h3 className="text-xl font-semibold mb-1">
-									{" "}
-									{project.title}
-								</h3>
-								<p className="text-muted-foreground text-sm mb-4">
-									{project.description}
-								</p>
-								<div className="flex justify-between items-center">
-									<div className="flex space-x-3">
-										<a
-											href={project.githubUrl}
-											target="_blank"
-											className="text-foreground/80 hover:text-primary transition-colors duration-300"
-										>
-											<Github size={20} />
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					))}
-				</div>
+                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
+                <p className="text-muted-foreground text-sm mb-4">
+                  {project.description}
+                </p>
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3">
+                    <a
+                      href={project.githubUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <Github size={20} />
+                    </a>
 
-				<div className="text-center mt-12">
-					<a
-						className="cosmic-button w-fit flex items-center mx-auto gap-2"
-						target="_blank"
-						href="https://github.com/Aashishkr2003/"
-					>
-						Check My Github <ArrowRight size={16} />
-					</a>
-				</div>
-			</div>
-		</section>
-	);
+                    {project.liveUrl && (
+                      <a
+                        href={project.liveUrl}
+                        target="_blank"
+                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                      >
+                        <ExternalLink size={20} />
+                      </a>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-12">
+          <a
+            className="cosmic-button w-fit flex items-center mx-auto gap-2"
+            target="_blank"
+            href="https://github.com/Aashishkr2003/"
+          >
+            Check My Github <ArrowRight size={16} />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
